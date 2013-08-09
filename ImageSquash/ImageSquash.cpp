cbab010ba00afb22b77a7ae77df2e4328cb58282
leeter <leeter@localhost>
2013-08-08 18:51:05 -0600

    move argument parsing to the boost library (new dependency) move the arguments out to their own class

--- ImageSquash/ImageSquash.cpp
+++ ImageSquash/ImageSquash.cpp
@@ -3,44 +3,47 @@
 
 #include "stdafx.h"
 #include "Output.h"
+#include "TransformInfo.h"
 #include "resource.h"
 
+namespace po = ::boost::program_options;
+namespace fs = ::boost::filesystem;
 #define E_WORKITEMQUEUEFAIL MAKE_HRESULT(SEVERITY_ERROR, 9, 1);
-
-typedef struct{
-	WCHAR inPath[MAX_PATH];
-	WCHAR outPath[MAX_PATH];
-	double dpi;
-	HRESULT hr;
-	LPCWSTR profilePath;
-} TransformInfo;
-
-
 
 static HRESULT DownSampleAndConvertImage(const TransformInfo&  info);
 static STDMETHODIMP CreateColorContextArray(IWICImagingFactory * factory, IWICColorContext *** toCreate, const UINT count);
 static DWORD WriteOutLastError();
 static DWORD WriteStdError(LPCWSTR error, size_t length);
-static HRESULT inline QueueFileForDownSample(const WIN32_FIND_DATA &file, LPCWSTR inPath, LPCWSTR outPath, LPCWSTR profilePath, double dpi, std::vector<TransformInfo> & worklist);
+static HRESULT inline QueueFileForDownSample(const WIN32_FIND_DATA &file, const std::wstring& inPath, const std::wstring& outPath, const std::wstring& profilePath, const double dpi, const ImageType type, std::vector<TransformInfo> & worklist);
 
+std::wistream& operator>>(std::wistream& in, ImageType & image)
+{
+    std::wstring token;
+    in >> token;
+    if (token == L"png")
+		image = PNG;
+    else if (token == L"jpg")
+		image = JPG;
+//    else throw boost::program_options::validation_error("Invalid unit");
+    return in;
+}
 
 /// <summary>program entry point</summary>
 int _tmain(int argc, _TCHAR* argv[])
 {
-	WCHAR profilePath[MAX_PATH] = {0};
-
+	std::cout.imbue(std::locale(""));
+	std::wcout.imbue(std::locale(""));
 	HRESULT hr = S_OK;
 	double dpi = 72.0;
-	size_t actuallength = 0;	
+	size_t actuallength = 0;
+	ImageType outputType = PNG;
 	std::vector<TransformInfo> worklist;
-	HANDLE consoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	HANDLE consoleIn = GetStdHandle(STD_INPUT_HANDLE);
+	//HANDLE consoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	//HANDLE consoleIn = GetStdHandle(STD_INPUT_HANDLE);
 	HANDLE standardError = GetStdHandle(STD_ERROR_HANDLE);
-
-	if(consoleOut == NULL)
-	{
-		return -1;
-	}
+	fs::wpath inputPath;
+	fs::wpath outputPath;
+	std::wstring profilePath;	
 
 	HMODULE module = ::GetModuleHandle(nullptr);
 	if (module == NULL)
@@ -49,108 +52,92 @@ int _tmain(int argc, _TCHAR* argv[])
 		return -1;
 	}
 
-	DWORD written = 0;
+	//DWORD written = 0;
 	WCHAR buffer[256] = {0};
 	int read = LoadString(module, Logo, buffer, 256);
-	if(!WriteConsole(consoleOut, buffer, read, &written, nullptr))
+	std::wcout << buffer << std::endl;
+
 	{
-		WriteOutLastError();
+
+	po::options_description desc("Options");
+	desc.add_options()
+		("help", "View full help")
+		("format,f", po::wvalue<ImageType>()->default_value(PNG, "png"), "output format")
+		("outPath,o", po::wvalue<std::wstring>()->required(), "output path")
+		("input,i", po::wvalue<std::wstring>()->required(), "input path")
+		("dpi", po::wvalue<double>(&dpi)->default_value(72.0, "72.0"), "output dpi");
+
+	po::variables_map vm;
+	po::wcommand_line_parser parser(argc, argv);
+	parser.options(desc);
+	po::store(parser.run(), vm);
+
+	if(vm.count("help"))
+	{
+		std::cout << desc << std::endl;
 		return -1;
 	}
-
-	for(int i = argc - 1; i; --i)
-	{
-		hr = ::StringCchLength(argv[i], 256, &actuallength);
-		if(!SUCCEEDED(hr) || actuallength > MAX_PATH)
-		{
-			return hr;
-		}
-
-		int actualLengthAsInt = static_cast<int>(actuallength);
-		if(::CompareStringOrdinal(argv[i], actualLengthAsInt, L"-?", 2, TRUE) == CSTR_EQUAL ||
-			::CompareStringOrdinal(argv[i], actualLengthAsInt, L"/?", 2, TRUE) == CSTR_EQUAL)
-		{
-			
-			read = ::LoadString(module, HelpString, buffer , 256);			
-			::WriteConsole(consoleOut, buffer, read, &written, NULL);
-			return 0;
-		}
-	}
-
-	if(argc < 2)
+	/*if(argc < 2)
 	{
 		read = ::LoadString(module, Error, &buffer[0], 256);
-		::WriteConsole(consoleOut, &buffer[0], read, &written, NULL);
+		::WriteConsole(consoleOut, &buffer[0], read, &written, nullptr);
 		return -1;
-	}
+	}*/
 
-	
-
-	/******************************************************************************
-	* Get the DPI if it's set at the command line
-	*/
-	for(int i = 1; i < argc; ++i)
+	if(vm.count("input"))
 	{
-		::StringCchLength(argv[i], 5, &actuallength);
-		if(::CompareStringOrdinal(argv[i], static_cast<int>(actuallength), L"-dpi", 4, TRUE) == CSTR_EQUAL)
-		{
-			++i;
-			if (i > argc)
-			{
-				return E_INVALIDARG;		
-			}
-			if (argv[i] != nullptr)
-			{
-				double result = 0.0;
-				std::wstring arg(argv[i]);
-				result = std::stod(arg);
-				if ((result != HUGE_VAL && !_isnan(result)) || result > 0)
-				{
-					dpi = result;
-				}
-			}		
-		}
+		inputPath = vm["input"].as<std::wstring>();
 	}
 
+	if(vm.count("outPath"))
+	{
+		outputPath = vm["outPath"].as<std::wstring>();
+	}
+
+	if(vm.count("format"))
+	{
+		outputType = vm["format"].as<ImageType>();
+	}
+	}
 	/**************************************************************************
 	 * Get the color directory once
 	 */
 	if (SUCCEEDED(hr))
 	{
+		WCHAR temp[MAX_PATH] = {0};
 		DWORD bufferSize = 0;
 		BOOL result = ::GetColorDirectory(nullptr, nullptr, &bufferSize);
 		// unfortunately unless we want to handle the pathing semantics we're
 		// with 8.3 paths
-		if (bufferSize == 0 || bufferSize > sizeof(profilePath))
+		if (bufferSize == 0 || bufferSize > sizeof(temp))
 		{
 			WriteOutLastError();
 			hr = E_FAIL;
 		}
-		::GetColorDirectory(NULL, profilePath , &bufferSize);
+		::GetColorDirectory(NULL, temp , &bufferSize);
+		profilePath = std::wstring(temp);
 	}
 
+	{
 	// all of this is is unnecessary in C++11 as we would have the <filesystem> header
 	WIN32_FIND_DATA file = {0};
 	WIN32_FIND_DATA outFile = {0};
-	HANDLE searchHandle = ::FindFirstFile(argv[1], &file);
-	HANDLE outSearchHandle = ::FindFirstFileEx(argv[2], FindExInfoBasic, &outFile, FindExSearchNameMatch, nullptr, NULL);
+	HANDLE searchHandle = ::FindFirstFile(inputPath.c_str(), &file);
+	HANDLE outSearchHandle = ::FindFirstFileEx(outputPath.c_str(), FindExInfoBasic, &outFile, FindExSearchNameMatch, nullptr, NULL);
 	if(file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && outFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 	{
-		WCHAR inPathBase[MAX_PATH] = {0};
-		WCHAR outPathBase[MAX_PATH] = {0};
-		::PathRemoveFileSpec(argv[1]);
-		::PathRemoveFileSpec(argv[2]);
-		::GetFullPathName(argv[1], MAX_PATH, inPathBase, nullptr);
-		::GetFullPathName(argv[2], MAX_PATH, outPathBase, nullptr);
+		fs::wpath inPathBase = fs::canonical(inputPath.remove_filename());
+		fs::wpath outPathBase = fs::canonical(outputPath.remove_filename());
+
 		while(::FindNextFile(searchHandle, &file))
 		{
-			WCHAR inPathBuffer[MAX_PATH] = {0};
-			WCHAR outPathBuffer[MAX_PATH] = {0};
+			fs::wpath inPathBuffer(inPathBase);
+			inPathBuffer /= file.cFileName;
 
-			::PathCombine(inPathBuffer, inPathBase, file.cFileName);
-			::PathCombine(outPathBuffer, outPathBase, file.cFileName);
+			fs::wpath outPathBuffer(outPathBase);
+			outPathBuffer /= file.cFileName;
 			
-			hr = QueueFileForDownSample(file, inPathBuffer, outPathBuffer, profilePath, dpi, worklist);
+			hr = QueueFileForDownSample(file, inPathBuffer.wstring(), outPathBuffer.wstring(), profilePath, dpi, outputType, worklist);
 			if(!SUCCEEDED(hr))
 			{
 				break;
@@ -159,8 +146,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	else
 	{
-		hr = QueueFileForDownSample(file, argv[1], argv[2], profilePath, dpi, worklist);
+		hr = QueueFileForDownSample(file, inputPath.wstring(), outputPath.wstring(), profilePath, dpi, outputType, worklist);
 	}
+
+	::FindClose(searchHandle);
+	::FindClose(outSearchHandle);
+	}
+
 	Concurrency::parallel_for_each(worklist.begin(), worklist.end(),
 		[](const TransformInfo & info){
 			HRESULT hr = ::CoInitializeEx(nullptr, COINIT_MULTITHREADED);
@@ -175,25 +167,16 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 	});
 
-	::FindClose(searchHandle);
-	::FindClose(outSearchHandle);
-
 	return hr;
 }
 
 /// <summary>Queues and image to be downsampled</summary>
-static HRESULT inline QueueFileForDownSample(const WIN32_FIND_DATA &file, LPCWSTR inPath, LPCWSTR outPath, LPCWSTR profilePath, double dpi, std::vector<TransformInfo> & worklist)
+static HRESULT inline QueueFileForDownSample(const WIN32_FIND_DATA &file, const std::wstring& inPath, const std::wstring& outPath, const std::wstring& profilePath, const double dpi, const ImageType type, std::vector<TransformInfo> & worklist)
 {
 	HRESULT hr = S_OK;
 	if(!(file.dwFileAttributes & (FILE_ATTRIBUTE_DEVICE | FILE_ATTRIBUTE_DIRECTORY | FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_VIRTUAL | FILE_ATTRIBUTE_TEMPORARY)))
 	{
-		TransformInfo info;
-		
-		StringCchCopy(info.inPath, MAX_PATH, inPath);
-		StringCchCopy(info.outPath, MAX_PATH, outPath);
-		info.dpi = dpi;
-		info.hr = S_OK;
-		info.profilePath = profilePath;
+		TransformInfo info(type, inPath, outPath, profilePath, dpi);
 
 		worklist.push_back(info);
 	}
@@ -243,7 +226,7 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 
 	if (SUCCEEDED(hr))
 	{
-		hr = pStream->InitializeFromFilename(info.inPath, GENERIC_READ);
+		hr = pStream->InitializeFromFilename(info.InputPath().c_str(), GENERIC_READ);
 	}	
 
 	if (SUCCEEDED(hr))
@@ -296,10 +279,10 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 
 
 	if (SUCCEEDED(hr) && !actualContexts && isCMYK)
-	{		
-		WCHAR finalPath[MAX_PATH];
-		PathCombine(finalPath, info.profilePath, L"RSWOP.icm");
-		hr = inputContexts[0]->InitializeFromFilename(finalPath);
+	{
+		fs::wpath finalPath(info.ProfilePath());
+		finalPath /= L"RSWOP.icm";
+		hr = inputContexts[0]->InitializeFromFilename(finalPath.wstring().c_str());
 	}
 
 	if (SUCCEEDED(hr) && finalCount)
@@ -314,9 +297,9 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 
 	if(SUCCEEDED(hr))
 	{
-		WCHAR finalPath[MAX_PATH];
-		PathCombine(finalPath, info.profilePath, L"sRGB Color Space Profile.icm");
-		hr = outputContexts[0]->InitializeFromFilename(finalPath);
+		fs::wpath finalPath(info.ProfilePath());
+		finalPath /= L"sRGB Color Space Profile.icm";
+		hr = outputContexts[0]->InitializeFromFilename(finalPath.wstring().c_str());
 	}
 
 	if (SUCCEEDED(hr) && inputContexts != nullptr)
@@ -344,7 +327,7 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 		hr = toOutput->GetResolution(&originalDpiX, &originalDpiY);
 	}
 
-	if (SUCCEEDED(hr) && (originalDpiX > info.dpi || originalDpiY > info.dpi))
+	if (SUCCEEDED(hr) && (originalDpiX > info.Dpi() || originalDpiY > info.Dpi()))
 	{
 		hr = pFactory->CreateBitmapScaler(&pScaler);
 	}	
@@ -352,8 +335,8 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 	UINT newSizeX = sizeX, newSizeY = sizeY;
 	if (SUCCEEDED(hr) && pScaler)
 	{
-		newSizeX = (UINT)ceil(((double)sizeX * info.dpi) / originalDpiX);
-		newSizeY = (UINT)ceil(((double)sizeY * info.dpi) / originalDpiY);
+		newSizeX = (UINT)ceil(((double)sizeX * info.Dpi()) / originalDpiX);
+		newSizeY = (UINT)ceil(((double)sizeY * info.Dpi()) / originalDpiY);
 		hr = pScaler->Initialize(toOutput, newSizeX, newSizeY, WICBitmapInterpolationModeFant);
 	}
 
@@ -365,8 +348,8 @@ static HRESULT DownSampleAndConvertImage(const TransformInfo&  info)
 
 	if(SUCCEEDED(hr))
 	{
-		ImageSquash::Output::OutputInfo outputInfo = { pFactory, toOutput, info.dpi, newSizeX, newSizeY};
-		hr = OutputImage(outputInfo, std::wstring(info.outPath), GUID_ContainerFormatPng);
+		ImageSquash::Output::OutputInfo outputInfo = { pFactory, toOutput, info.Dpi(), newSizeX, newSizeY};
+		hr = OutputImage(outputInfo, info.OutputPath(), GUID_ContainerFormatPng);
 	}
 	
 	// cleanup factory
@@ -409,7 +392,7 @@ static DWORD WriteOutLastError()
 
 static DWORD WriteStdError(LPCWSTR error, size_t length)
 {
-	HANDLE stdError = GetStdHandle(STD_ERROR_HANDLE);
+	HANDLE stdError = ::GetStdHandle(STD_ERROR_HANDLE);
 	if (stdError != INVALID_HANDLE_VALUE)
 	{
 		size_t actualLength = 0;
@@ -429,7 +412,7 @@ static STDMETHODIMP CreateColorContextArray(IWICImagingFactory * factory, IWICCo
 {
 	HRESULT hr = S_OK;
 	*toCreate = new (std::nothrow) IWICColorContext*[count];
-	if (*toCreate == NULL)
+	if (*toCreate == nullptr)
 	{
 		hr = E_OUTOFMEMORY;
 		return hr;
---
+++ ImageSquash/TransformInfo.cpp
@@ -0,0 +1,13 @@
+#include "StdAfx.h"
+#include "TransformInfo.h"
+
+
+TransformInfo::TransformInfo(const ImageType type, const std::wstring& inPath, const std::wstring& outPath, const std::wstring & profilePath, const double dpi)
+	:imageType(type), inputPath(inPath), outputPath(outPath), profilePath(profilePath), dpi(dpi)
+{
+}
+
+
+TransformInfo::~TransformInfo(void)
+{
+}
